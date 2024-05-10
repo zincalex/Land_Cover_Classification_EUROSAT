1199082613311fbb2485e507e6b3734b08e5dec6
Francesco Colla <francescocolla1801@gmail.com>
2024-05-10 11:40:47 +0200

    more tests, changed main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 # All resnet50 pretrained models require 3 x 224 x 224 sizes
 # Visto che il data set è diviso in cartelle, noi facciamo che prendiamo una cartella e prendiamo x% delle prime immagini
+# Per ogni classe, creare le matrici di training con valore + label -----> assegnare noi le label
+# fare shuffle una volta preso tutto il training set
 
 # based 
 import os               # paths
 import numpy as np      # arrays 
-#from osgeo import gdal  
+import matplotlib.pyplot as plt 
+#from osgeo import gdal  from tif to rgb
 from tqdm import tqdm   # progress bar
 
 # torch
@@ -27,10 +30,18 @@ print(f"Using {DEVICE} device")
 def main () : 
     folders_path = '../dataset/'
     classes = os.listdir(folders_path) 
+    dict_class = {}
+    for i in range(len(classes)) : 
+        dict_class.update({classes[i] : i + 1})
+
+    num_classes = len(classes)
+    bands = 13
+
     print(classes)
+    print(dict_class)
 
-
-    resnet50(weights=ResNet50_Weights.DEFAULT)
+    # RESNET50
+    model = resnet50(weights = ResNet50_Weights.DEFAULT)
 
 
 
--- test.py
+++ test.py
@@ -1,5 +1,24 @@
 import torch
+import matplotlib.pyplot as plt
+from PIL import Image
+import cv2
+
 x = torch.rand(5, 3)
-print(x)
+#print(x)
+
+image_path = r'C:\GitHub\DL_project\dataset\AnnualCrop\AnnualCrop_235.tif'
+
+img = cv2.imread(image_path, cv2.IMREAD_ANYDEPTH | cv2.IMREAD_ANYCOLOR)
+
+if img is not None:
+    print("Dimensioni dell'immagine:", img.shape)
+else:
+    print("Impossibile aprire l'immagine.")
+
+
+#img = Image.open('C:\GitHub\DL_project\dataset\AnnualCrop\example_TIFF_1MB.tif')
+
+#plt.imshow(img)
+#plt.show()
 
 
