3fbb0c2df6e2bdccb82b46031effb4cd8750292e
Francesco Colla <francescocolla1801@gmail.com>
2024-05-11 14:02:35 +0200

    added tests

--- test.py
+++ test.py
@@ -1,24 +1,53 @@
+# All resnet50 pretrained models require 3 x 224 x 224 sizes
+# Visto che il data set è diviso in cartelle, noi facciamo che prendiamo una cartella e prendiamo x% delle prime immagini
+# Per ogni classe, creare le matrici di training con valore + label -----> assegnare noi le label
+# fare shuffle una volta preso tutto il training set
+
+# based 
+import os               # paths
+import numpy as np      # arrays 
+import matplotlib.pyplot as plt 
+#from osgeo import gdal  from tif to rgb
+from tqdm import tqdm   # progress bar
+
+# torch
 import torch
-import matplotlib.pyplot as plt
-from PIL import Image
-import cv2
-
-x = torch.rand(5, 3)
-#print(x)
-
-image_path = r'C:\GitHub\DL_project\dataset\AnnualCrop\AnnualCrop_235.tif'
-
-img = cv2.imread(image_path, cv2.IMREAD_ANYDEPTH | cv2.IMREAD_ANYCOLOR)
-
-if img is not None:
-    print("Dimensioni dell'immagine:", img.shape)
-else:
-    print("Impossibile aprire l'immagine.")
+from torchvision.models import resnet50, ResNet50_Weights
 
 
-#img = Image.open('C:\GitHub\DL_project\dataset\AnnualCrop\example_TIFF_1MB.tif')
+DEVICE = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+torch.set_default_device(DEVICE)
+print(f"Using {DEVICE} device")
 
-#plt.imshow(img)
-#plt.show()
 
 
+def main () : 
+    folders_path = '../dataset/'
+    classes = os.listdir(folders_path) 
+    dict_class = {}
+    for i in range(len(classes)) : 
+        dict_class.update({classes[i] : i + 1})
+
+    num_classes = len(classes)
+    bands = 13
+
+    
+    print(classes)
+    print(dict_class)
+
+    # RESNET50
+    model = resnet50(weights = ResNet50_Weights.DEFAULT)
+
+def taskb ():
+    
+
+
+
+if __name__ == '__main__':
+    main()
