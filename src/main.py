35b0669c39aa00f8ad52e968e29a4fb12a121341
zincalex <92792074+zincalex@users.noreply.github.com>
2024-05-14 19:10:51 +0200

    Dataset creation generalization; Documentation partially added to few methods; Directory problems on parameters saving resolved;

--- src/main.py
+++ src/main.py
@@ -1,13 +1,10 @@
 # TODO chiedere al prof quale sia migliore/convenzione tra patterns, instances, samples, etc....
 # TODO chiedere al prof perchè la prima run vada così lenta nel buildare il dataset
 # TODO come è possibile che nel paper hanno fatto analisi resnet su solo 2 bande o 4 bande
-# fixare if che controlla de nel dataset ci sono anche altri file o solo directory
+# TODO introdurre possibile salvataggio del dataset 13 canali in file csv
 # dimensionality reduction, PCA
 
 # All resnet50 pretrained models require 3 x 224 x 224 sizes
-# Visto che il data set è diviso in cartelle, noi facciamo che prendiamo una cartella e prendiamo x% delle prime immagini
-# Per ogni classe, creare le matrici di training con valore + label -----> assegnare noi le label
-# fare shuffle una volta preso tutto il training set
 
 # based 
 import os               # paths
@@ -25,18 +22,16 @@ from torch.utils.data import Dataset
 from torchvision import transforms, datasets
 from torchvision.models import resnet50, ResNet50_Weights
 
+
+
 parser = argparse.ArgumentParser()
-#-t ANALYSIS TYPE
 parser.add_argument("-t", type = int, help="Analysis type", default = 0)
 args = parser.parse_args()
 
-
 ANALYSIS = args.t
 DEVICE = (
-    "cuda" 
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
+    "cuda" if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available()
     else "cpu"
 )
 torch.set_default_device(DEVICE)
@@ -56,38 +51,19 @@ class EuroSATDataset(Dataset) :
         labels (list): list of labels
         transform (callable): transform to apply to the instances
     """
-    def __init__(self, instances, labels, transform, dataset = None, subset_bands = []):
-        
-        if subset_bands and dataset is not None :
-            img_list, label_list = np.array([]),  np.array([])
-
-            with tqdm(total=len(dataset), unit='instance') as inpbar:      
-                for i in range(len(dataset)) :
-                    data = dataset.__getchanneles__(i, subset_bands)
-                    img_list = np.append(img_list, data[0])
-                    label_list = np.append(img_list, data[1])
-                    inpbar.update(1)
-            self.instances =  img_list
-            self.labels = label_list
-            self.transform = transform  
-             
-
-        else :
-            self.labels = labels                     # classes
-            self.instances = instances               # images
-            self.transform = transform               # transformations
+    def __init__(self, instances, labels, transform):
+        self.labels = labels                     # classes
+        self.instances = instances               # images
+        self.transform = transform               # transformations
 
     def __len__(self) :
         return len(self.instances)
     
     def __getitem__(self, idx) :
         return self.transform(self.instances[idx]), self.labels[idx] 
-    
-    def __getitems(self) :
-        return self.instances, self.labels
-    
-    def __getchanneles__(self, idx, bands_selected) :
-        return self.transform(self.instances[idx][:,:,bands_selected]), self.labels[idx] 
+
+    def __getchannelesvalue__(self, bands_selected) :
+        return self.transform(self.instances[:,:,bands_selected]), self.labels[:]
 
 
 class EncoderDecoderCNN(nn.Module) :
@@ -126,31 +102,41 @@ def load_tif_channels(img_path, bands_selected):
 
 
 def load_data(img_path):
-    """
+    """Read the an image through the tifffile library and convert the values
+       of the image from uint16 to float32 
+
+    Args:
+        img_path: the path to a .tif image
+
+    Returns:
+        The image as numpy array
     
     """
     img_np_array = tifffile.imread(img_path)
-    img_np_array = img_np_array.astype(np.float32)
-    return img_np_array
+    return img_np_array.astype(np.float32)
 
-def save_model(net, path):
-    """
-    
+
+def save_model_parameters(model, model_parameters_path):
+    """Save the parameters of the model inside the specified path in a .pth file
+
+    Args:
+        model:                     type of NN used
+        model_parameters_path:     filesystem location and name of the file to be saved
     """
     print("\nSaving model...")
-    if not os.path.exists('pretrained') :
-            os.makedirs('pretrained')
-    torch.save(net.state_dict(), path)
-    print(f'MODEL SAVED to {path}')
+    if not os.path.exists(model_parameters_path) :
+            os.makedirs(model_parameters_path)
+    torch.save(model.state_dict(), model_parameters_path)
+    print(f'MODEL SAVED to {model_parameters_path}')
 
 
 
 
-def resnet50_training(model, train_data, lf, optimizer, epochs, model_name):
+def resnet50_training(model, train_data, lf, optimizer, epochs, bands_name):
     """
 
     """
-    pretrained_model_path = f'../pretrained/{model_name}.pth'
+    model_parameters_path = f'../parameters/{bands_name}.pth'
     for epoch in range(epochs):    
         with tqdm(total=len(train_data), unit='instance') as inpbar:
             for data in train_data:
@@ -160,13 +146,12 @@ def resnet50_training(model, train_data, lf, optimizer, epochs, model_name):
                 outputs = model(images)
                 loss = lf(outputs, labels)
 
-                # backward pass
-                loss.backward()
+                loss.backward()  # Backward pass
                 optimizer.step()
                 inpbar.update(1)
         print(f'Training loss: {loss.item()}          epoch: {epoch}\n')
     
-    save_model(model, pretrained_model_path)
+    save_model_parameters(model, model_parameters_path)
 
 
 
@@ -183,8 +168,6 @@ def main () :
     lr = 1e-4                                                           # learning rate
     factor = 20                                                         # learning rate factor for tuning
     epochs = 3                                                          # fixed number of epochs
-    #bands_selected = [0,1,2,3,4,5,6,7,8,9,10,11,12]                    # bands selected to analyze 3 2 1 is RGB 
-    bands_selected = [3,2,1]
     subset_bands = [[3,2,1], [0, 8, 9]]
     subset_names = ['RGB', 'Atmosperic_Factors']
     pretrained_flag = 0
@@ -199,44 +182,55 @@ def main () :
     for i in range(len(labels)) : 
         dict_class.update({labels[i] : i + 1})
 
-
-    
-    print("DATASET CREATION")
+    print("CREATING DATASET...")
     train_instances, train_label, test_instances, test_label = [], [], [], []
     with tqdm(total=num_classes,  unit='label') as pbar:
         for label in os.listdir(dataset_path) :
             label_dir = os.path.join(dataset_path, label)
             images = os.listdir(label_dir)
 
-            m_training = int(len(images) * fraction_train)
+            m_training = int(len(images) * fraction_train)          # Training set
             for i in range(m_training) : 
                 img_path = label_dir + '/' + images[i]
                 train_instances.append(load_data(img_path))
                 train_label.append(dict_class[label])
 
-            for i in range(m_training, len(images)) :               # the remaining images are for the test set 
+            for i in range(m_training, len(images)) :               # Test set
                 img_path = label_dir + '/' + images[i]
                 test_instances.append(load_data(img_path))
                 test_label.append(dict_class[label])
             pbar.update(1)
 
-        
-    # Define transformations to apply to the images (e.g., resizing, normalization)
-    transform = transforms.Compose([
+    transform = transforms.Compose([        # Define transformations to apply to the images (e.g., resizing, normalization)
         transforms.ToPILImage(),
-        transforms.Resize(256),       # Resize images to 256x256                                                          
-        transforms.CenterCrop(224),  # Crop the images to our desired size
-        transforms.ToTensor(),        # Convert images to PyTorch tensors (standardization is automatically applied)                                                                                                                                                                           
+        transforms.Resize(256),             # Resize images to 256x256                                                          
+        transforms.CenterCrop(224),         # Crop the images 224x224, required by resnet50
+        transforms.ToTensor(),              # Convert images to PyTorch tensors (standardization automatically applied)                                                                                                                                                                           
         #transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])                        # Normalize images
     ])
     
-    train_dataset = EuroSATDataset(train_instances, train_label, transform)  
-    test_dataset = EuroSATDataset(test_instances, test_label, transform)  
+    # Main dataset, 13 channels
+    train_dataset = EuroSATDataset(np.array(train_instances), train_label, transform)  
+    test_dataset = EuroSATDataset(np.array(test_instances), test_label, transform)  
+
     train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size = batch_size, shuffle = True, generator = g_device)
     test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size = batch_size, shuffle = True, generator = g_device)
-    print("DATASET CREATION COMPLETE")
+
+    print("DATASET CREATION COMPLETE\n\n")
     
+
     if (ANALYSIS == 1) : 
+        print("Starting analysis: different resnet50s are trained on different band combinations; the outputs are weighted before producing an output")
+
+        print("Creating dataset subsets...")
+        # Subdataset with lesser channels
+        train_dataset_RGB = EuroSATDataset(np.array(train_instances)[:,:,:,subset_bands[0]], train_label, transform)
+        train_dataset_atmosferic = EuroSATDataset(np.array(train_instances)[:,:,:,subset_bands[1]], train_label, transform)  
+
+
+        RGB_channels_train_data_loader = torch.utils.data.DataLoader(train_dataset_RGB, batch_size = batch_size, shuffle = True, generator = g_device)
+        atmos_channels_train_data_loader = torch.utils.data.DataLoader(train_dataset_atmosferic, batch_size = batch_size, shuffle = True, generator = g_device)
+        print("Complete")
         
         for i, sub_bands in enumerate(subset_bands) :
             model = resnet50(weights = None)
@@ -245,16 +239,14 @@ def main () :
             loss_funct = nn.CrossEntropyLoss()
             optimizer = torch.optim.Adam(model.parameters(), lr=lr)         #optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
 
-            print("Starting channel subsampling on dataset")
-            lesser_channel_training_dataset = EuroSATDataset([], [], transform = transform, dataset=train_dataset, subset_bands = sub_bands)
-            lesser_channel_train_data_loader = torch.utils.data.DataLoader(lesser_channel_training_dataset, batch_size = batch_size, shuffle = True, generator = g_device)
-            print("Finished channel subsampling on dataset")
-            print(f"Starting Training resnet50 number {i} on {subset_names[i]}")
-            resnet50_training(model=model, train_data = lesser_channel_train_data_loader, lf=loss_funct, optimizer=optimizer, epochs=epochs, model_name=subset_names[i])
+            print(f"Starting Training on resnet50 number {i+1} on {subset_names[i]} bands")
+            resnet50_training(model=model, train_data = RGB_channels_train_data_loader, lf=loss_funct, optimizer=optimizer, epochs=epochs, bands_name=subset_names[i])
+
+
     elif( ANALYSIS == 2) : 
-        print("sus")
+        print("Starting analysis: layers are added before resnet50 for channel reduction from 13 to 3")
     elif( ANALYSIS == 3) :
-        print("sus")
+        print("Starting analysis: encoder-decoder structure for channel reduction on the dataset. Resnet50 is then trained")
     else :
         print("suca")
         exit(1)
